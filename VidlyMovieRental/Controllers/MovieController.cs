8e55743ded18e874bd8436a0bd5ca2051686c63c
SammyK08 <samrajkarki08@gmail.com>
2019-11-13 04:21:52 -0500

    adding the migration
    We used the code first migration to create the database.

---
+++ VidlyMovieRental/Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMovieRental.Models;
+using VidlyMovieRental.ViewModel;
+
+namespace VidlyMovieRental.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() { Id = 1, Name = "mad max" };
+
+            var customers = new List<Customer>()
+            {
+                new Customer{Id=1, Name="samraj"},
+                new Customer{Id=2, Name="beeva"}
+            };
+
+            var viewModel = new RandomViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ VidlyMovieRental/ViewModel/RandomViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyMovieRental.Models;
+
+namespace VidlyMovieRental.ViewModel
+{
+    public class RandomViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
