1b5b37e0d22a784ad1f103e7f4befb4f07443488
SammyK08 <samrajkarki08@gmail.com>
2019-11-13 04:59:54 -0500

    apply annotation to Customer Name

---
+++ VidlyMovieRental/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMovieRental.Models;
+
+namespace VidlyMovieRental.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+           
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = _context.Customers.ToList();
+            return View(customer);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+                           
+    }
+}
