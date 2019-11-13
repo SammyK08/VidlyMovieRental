585fe7bd88b1faf7db37ec278ea34c269f20b333
SammyK08 <samrajkarki08@gmail.com>
2019-11-13 08:36:34 -0500

    Adding MembershipType to list of customer

---
+++ VidlyMovieRental/Migrations/201911131318459_ChangeInMembershipType.cs
@@ -0,0 +1,24 @@
+﻿namespace VidlyMovieRental.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeInMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+
+            Sql("UPDATE MembershipTypes set Name='pay as you go' Where Id=1 ");
+            Sql("UPDATE MembershipTypes set Name='monthly' Where Id=2 ");
+            Sql("UPDATE MembershipTypes set Name='quaterly' Where Id=3 ");
+            Sql("UPDATE MembershipTypes set Name='yearly' Where Id=4 ");
+        }
+
+
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
