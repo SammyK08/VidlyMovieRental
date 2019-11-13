02af1ceca63c147d9c604dd47ba97bc9fd50472d
SammyK08 <samrajkarki08@gmail.com>
2019-11-13 08:10:07 -0500

    Eager Loadig
    Along with the customer information we displayed the information about the membership type from another table by using the eager loading.

--- VidlyMovieRental/Controllers/CustomerController.cs
+++ VidlyMovieRental/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,13 +27,16 @@ namespace VidlyMovieRental.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-            var customer = _context.Customers.ToList();
+            var customer = _context.Customers.
+                           Include(c => c.MembershipType).
+                           ToList();
+
             return View(customer);
         }
-
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.
+                            SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
